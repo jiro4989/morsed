0efb4fb29d65743bd4d65bfcee0d2d3e7ae3f7d2
jiro4989 <jiroron666@gmail.com>
2020-11-14 00:27:10 +0900

    feat: example code

--- src/morsed/core.clj
+++ src/morsed/core.clj
@@ -1,9 +1,27 @@
 (ns morsed.core
   (:import [com.atilika.kuromoji.ipadic Token Tokenizer]))
 
-(defn example []
-  (let [tok (Tokenizer.)]
-    (println (.tokenize tok "お寿司が食べたい。"))))
+(defn tokens [^String s]
+  (.tokenize (Tokenizer.) s))
+
+(defn part-replace [token part sub]
+  (if (= (.getPartOfSpeechLevel1 token) part)
+    sub
+    (.getSurface token)))
+
+(defn convert [text part sub]
+  (->> (tokens text)
+       (map #(part-replace % part sub))
+       (apply str)))
+
+(defn pp [text part sub]
+  (println text " ---> " (convert text part sub)))
 
 (defn -main []
-  (example))
+  (do
+    (pp "吾輩は猫である。名前はまだない。" "名詞" "寿司")
+    (pp "右の頬を殴られたら左の頬を差し出せ" "名詞" "寿司")
+    (pp "彼のカレー" "名詞" "寿司")
+    (pp "このひき肉は引きにくいひき肉" "名詞" "寿司")
+    (pp "隣の客はよく柿食う客だ" "名詞" "寿司")
+  ))
