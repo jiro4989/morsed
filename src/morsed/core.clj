e15f7511686008a9bdf07a7f5061e21b93e736ad
jiro4989 <jiroron666@gmail.com>
2020-11-13 22:58:00 +0900

    feat: update sample code

--- src/morsed/core.clj
+++ src/morsed/core.clj
@@ -1,6 +1,11 @@
-(ns morsed.core)
+(ns morsed.core
+  (:import [org.atilika.kuromoji Token Tokenizer]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn example []
+  (let [tokenizer (.build (Tokenizer/builder))]
+    (doall
+     (for [token (.tokenize tokenizer "寿司が食べたい。")]
+       (println (.getSurfaceForm token) "\t" (.getAllFeatures token))))))
+
+(defn -main []
+  (example))
