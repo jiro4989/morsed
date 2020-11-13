796b372cb18a263b118dd48506e119d89331a9b4
jiro4989 <jiroron666@gmail.com>
2020-11-14 01:12:44 +0900

    feat: update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
 (defproject morsed "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/jiro4989/morsed"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  ; :repositories {"Atilika Open Source repository" "https://www.atilika.org/nexus/content/repositories/atilika"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.atilika.kuromoji/kuromoji-ipadic "0.9.0"]
                  [org.clojure/tools.cli "1.0.194"]]
   :repl-options {:init-ns morsed.core}
-  :main "morsed.core")
+  :main "morsed.core"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+                       :prep-tasks ["compile"]
+                       :uberjar-name "morsed.jar"}}
+  )
