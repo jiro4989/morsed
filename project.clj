15e4197c6bf14bc55ede7787218221cb57ef2476
jiro4989 <jiroron666@gmail.com>
2020-11-14 00:45:55 +0900

    feat: add dependencies

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   ; :repositories {"Atilika Open Source repository" "https://www.atilika.org/nexus/content/repositories/atilika"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.atilika.kuromoji/kuromoji-ipadic "0.9.0"]]
+                 [com.atilika.kuromoji/kuromoji-ipadic "0.9.0"]
+                 [org.clojure/tools.cli "1.0.194"]]
   :repl-options {:init-ns morsed.core}
   :main "morsed.core")
