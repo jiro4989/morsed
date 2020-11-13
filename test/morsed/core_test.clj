d0bf4998fa4e51f3a5923ea495ec8d3ba9bb5260
jiro4989 <jiroron666@gmail.com>
2020-11-14 01:29:19 +0900

    chore: add test cases

--- test/morsed/core_test.clj
+++ test/morsed/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [morsed.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest convert-test
+  (testing "ok"
+    (is (= "寿司は寿司である" (convert "吾輩は猫である" "名詞" "寿司")))))
+
+(deftest do-main-test
+  (testing "ok"
+    (is (nil? (do-main ["吾輩は猫である"] {:part "名詞" :sub "寿司"})))))
